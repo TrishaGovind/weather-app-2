91b2fd2f3b5c8ac836d0629f36347ebbb06eb1b3
TrishaGovind <trisha.govind@gmail.com>
2022-01-17 12:38:43 +0800

    Update css

--- src/Weather.js
+++ src/Weather.js
@@ -4,41 +4,73 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
+      <div className="container-main">
+        <form>
+          <input
+            type="text"
+            placeholder="Enter a city"
+            className="form-control"
+            autoComplete="off"
+          />
+          <input type="submit" value="Search" className="btn btn-primary" />
+        </form>
+        <div className="row mt-3">
+          <div className="col-4 ps-5 pt-3">
+            <h1>Perth</h1>
+            <h5>Australia</h5>
+          </div>
+          <div className="col-4">
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt="clear sky"
+              className="center-block"
+              width="100"
             />
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              placeholder="Search"
-              className="btn btn-primary"
-            />
+
+          <div className="col-4 mt-2 pe-5 pt-3">
+            <ul>
+              <li>Monday</li>
+              <li>17 January 2022</li>
+              <li>11:11</li>
+            </ul>
           </div>
         </div>
-      </form>
-      <h1>Perth</h1>
-      <h6>Australia</h6>
-      <ul>
-        <li>Monday, 17 January 2022</li>
-        <li>Clear</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Clear" />
-          28º
+        <h2>28ºC</h2>
+        <h6>CLEAR SKY</h6>
+      </div>
+      <div class="row prevision-container">
+        <div class="col card-container">
+          <div class="card">
+            <div class="card-body">
+              <h6>FEELS LIKE</h6>
+              <p>
+                <span id="feels-like">27</span>º
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 2%</li>
-            <li>Wind: 2km/h</li>
-          </ul>
+
+        <div class="col card-container">
+          <div class="card">
+            <div class="card-body">
+              <h6>HUMIDITY</h6>
+              <p>
+                <span id="humidity">44</span>%
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col card-container">
+          <div class="card">
+            <div class="card-body">
+              <h6>WIND</h6>
+              <p>
+                <span id="wind-speed">6</span>km/h
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
