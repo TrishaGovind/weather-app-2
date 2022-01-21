182377115220cdd1edcd89973fe037d04b32b9ad
TrishaGovind <trisha.govind@gmail.com>
2022-01-21 17:28:51 +0800

    Attempt to deploy on netlify

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="WHITE"
-      size={40}
+      size={45}
       animate={true}
     />
   );
